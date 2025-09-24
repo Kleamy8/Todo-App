8092de21bbdfb3a63cce2e263b3f8d6275f408d9
Kleamy8 <remanrada8@gmail.com>
2025-09-24 13:34:20 +0200

    Delete backend directory
    Vi glömde använda oss utav github actions

--- backend/src/model/userModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const UserSchema = mongoose.Schema({
-  name: { type: String },
-  age: { type: Number },
-  email: { type: String },
-  password: { type: String },
-});
-module.exports = mongoose.model("User", UserSchema);
