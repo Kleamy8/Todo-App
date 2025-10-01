5d61664129da65c69592cdd842ee5397731c976d
amir23060 <amir23060@gmail.com>
2025-10-01 14:17:19 +0200

    changed tests

--- src/components/home/home.spec.tsx
+++ src/components/home/home.spec.tsx
@@ -1,18 +1,1 @@
-import { render, screen } from "@testing-library/react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Home } from "./home";
-import { Header } from "./header";
-
-describe("HomePage", () => {
-  it("should renders a title", () => {
-    render(<Home />);
-
-    // act
-
-    const button = screen.getByTestId("addnewbutton");
-    // assert
-    expect(
-      screen.getByRole("button", { name: /add new todo/i })
-    ).toBeInTheDocument();
-  });
-});
+export {};
