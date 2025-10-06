cdfadd40ab6c1650a6fe95e6771617bd2f791935
amir23060 <amir23060@gmail.com>
2025-10-06 15:15:59 +0200

    made category dropbox

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./home.module.css";
 export function Header({ name }: { name: string }) {
+  const [show, setShow] = useState(false);
+  const [category, setCategory] = useState("");
+  useEffect(() => {
+    localStorage.clear();
+    localStorage.setItem("category", category);
+  }, [category]);
   return (
     <>
       <div className={styles.header}>
@@ -8,7 +14,47 @@ export function Header({ name }: { name: string }) {
           <h1>{name}</h1>
         </div>
         <div className={styles.navbar}>
-          <p>About us</p>
+          <div>
+            <p onClick={() => setShow(!show)}>
+              {category !== "" ? category : "Category"}
+            </p>
+            {show && (
+              <ul>
+                <li
+                  onClick={() => {
+                    setCategory("");
+                    setShow(false);
+                  }}
+                >
+                  All
+                </li>
+                <li
+                  onClick={() => {
+                    setCategory("Study");
+                    setShow(false);
+                  }}
+                >
+                  Study
+                </li>
+                <li
+                  onClick={() => {
+                    setCategory("Work");
+                    setShow(false);
+                  }}
+                >
+                  Work
+                </li>
+                <li
+                  onClick={() => {
+                    setCategory("Private");
+                    setShow(false);
+                  }}
+                >
+                  Private
+                </li>
+              </ul>
+            )}
+          </div>
           <p>Contact us</p>
         </div>
       </div>
